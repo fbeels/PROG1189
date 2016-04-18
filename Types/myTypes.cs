5a374f29303d765dd1a39246a458883b3b32194b
Cedric Robichaud <ceddie@live.ca>
2016-04-18 13:37:40 -0300

    added parmstructure

--- Types/myTypes.cs
+++ Types/myTypes.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics;
 
 namespace MyTypes
 {
-   public enum ItemStatus
+    public enum ItemStatus
     {
         Pending,
         Approved,
@@ -19,4 +21,46 @@ namespace MyTypes
         UnderReview,
         Closed
     }
+
+
+    public struct ParmStructure
+    {
+        private string ParmID_;
+        private SqlDbType ParmDataType_;
+        private int ParmSize_;
+        private ParameterDirection ParmDirection_;
+        private object ParmValue_;
+        public string ParmID
+        {
+            get { return ParmID_; }
+        }
+        public SqlDbType ParmDataType
+        {
+            get { return ParmDataType_; }
+        }
+        public int ParmSize
+        {
+            get { return ParmSize_; }
+        }
+        public System.Data.ParameterDirection ParmDirection
+        {
+            get { return ParmDirection_; }
+        }
+        public object ParmValue
+        {
+            get { return ParmValue_; }
+            set { ParmValue_ = value; }
+        }
+
+
+        public ParmStructure(string myParmID, SqlDbType myParmDataType, ParameterDirection myParmDirection, int myParmSize = 0, object myParmValue = null)
+        {
+            ParmID_ = myParmID;
+            ParmDataType_ = myParmDataType;
+            ParmSize_ = myParmSize;
+            ParmDirection_ = myParmDirection;
+            ParmValue_ = myParmValue;
+        }
+    }
+
 }
