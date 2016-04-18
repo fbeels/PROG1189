e472926159eb2ed3599c228ea4b990874d91ab7e
Cedric Robichaud <ceddie@live.ca>
2016-04-18 13:01:56 -0300

    added enums for purchase orders and their items

---
+++ Types/myTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+   public enum ItemStatus
+    {
+        Pending,
+        Approved,
+        Denied
+    }
+
+    public enum OrderStatus
+    {
+        Pending,
+        UnderReview,
+        Closed
+    }
+}
